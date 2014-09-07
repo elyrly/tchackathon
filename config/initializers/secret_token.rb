85fed8829d41553e3ab1f5377678333612953dc7
Richard Yang <elyrly@gmail.com>
2014-09-06 22:46:49 -0700

    commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Tchackathon::Application.config.secret_key_base = '274bdb306195d6a5c6f1c8d9d06a31a98b65ee16a64e4929b139777e3e93d563a65787a49255f781a3736c5ab4e0c327f5eb68308fe9f5da6c2c901a47991af1'
